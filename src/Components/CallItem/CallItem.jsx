e7884cd0342f7bc3c80d4545dd5db8f6725c5cdb
k2pbac <krisbachan123@hotmail.com>
2021-11-18 13:47:48 -0500

    add images icons for call type and ellipsis, update component style

--- src/Components/CallItem/CallItem.jsx
+++ src/Components/CallItem/CallItem.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
 import "./CallItem.css";
-
+import outboundCall from "./outbound.png";
+import inboundCall from "./inbound.png";
 const CallItem = () => {
   return (
     <div className="call_item">
-      <span>Icon</span>
+      <img className="call_item__icon-outbound" src={outboundCall} />
       <div className="call_item__details">
-        <p>+33 6 45 13 53 91</p>
-        <p>tried to call on Xavier</p>
+        <span>
+          <p>+33 6 45 13 53 91</p>
+          <p>tried to call on Xavier</p>
+        </span>
       </div>
       <div className="call_item__time">
         <p>
-          07:58 <span>pm</span>
+          <FontAwesomeIcon size="1x" icon={faEllipsisV} />
+          07:58 <span>PM</span>
         </p>
       </div>
     </div>
