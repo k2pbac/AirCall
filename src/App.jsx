6eebbed1efb6c4e26cc3077814d1d392053b1756
k2pbac <krisbachan123@hotmail.com>
2021-11-18 11:49:07 -0500

    implement basic jsx and css for call item with hardcoded data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import CallItem from "./Components/CallItem/CallItem.jsx";
 
-import Header from './Header.jsx';
+import Header from "./Header.jsx";
 
 const App = () => {
   return (
-    <div className='container'>
-      <Header/>
-      <div className="container-view">Some activities should be here</div>
+    <div className="container">
+      <Header />
+      <div className="container-view">
+        <CallItem />
+      </div>
     </div>
   );
 };
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
 
 export default App;
--- src/Components/CallItem/CallItem.jsx
+++ src/Components/CallItem/CallItem.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import "./CallItem.css";
+
 const CallItem = () => {
-  <div></div>;
+  return (
+    <div className="call_item">
+      <span>Icon</span>
+      <div className="call_item__details">
+        <p>+33 6 45 13 53 91</p>
+        <p>tried to call on Xavier</p>
+      </div>
+      <div className="call_item__time">
+        <p>
+          07:58 <span>pm</span>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CallItem;
