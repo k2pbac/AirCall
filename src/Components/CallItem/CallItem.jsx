e76aac343b4be12076ec756527296c23d81fe9b2
k2pbac <krisbachan123@hotmail.com>
2021-11-18 14:44:56 -0500

    add date on top of call details section, add styles to center between lines

--- src/Components/CallItem/CallItem.jsx
+++ src/Components/CallItem/CallItem.jsx
@@ -7,18 +7,23 @@ import inboundCall from "./inbound.png";
 const CallItem = () => {
   return (
     <div className="call_item">
-      <img className="call_item__icon-outbound" src={outboundCall} />
-      <div className="call_item__details">
-        <span>
-          <p>+33 6 45 13 53 91</p>
-          <p>tried to call on Xavier</p>
-        </span>
+      <div className="call_item__date">
+        <span>JULY, 21 2017</span>
       </div>
-      <div className="call_item__time">
-        <p>
-          <FontAwesomeIcon size="1x" icon={faEllipsisV} />
-          07:58 <span>PM</span>
-        </p>
+      <div className="call_item__information">
+        <img className="call_item__icon-outbound" src={outboundCall} />
+        <div className="call_item__details">
+          <span>
+            <p>+33 6 45 13 53 91</p>
+            <p>tried to call on Xavier</p>
+          </span>
+        </div>
+        <div className="call_item__time">
+          <p>
+            <FontAwesomeIcon size="1x" icon={faEllipsisV} />
+            07:58 <span>PM</span>
+          </p>
+        </div>
       </div>
     </div>
   );
