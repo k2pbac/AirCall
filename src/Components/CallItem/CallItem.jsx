12b2a62571c2f51703509c40917e25fb87be508e
k2pbac <krisbachan123@hotmail.com>
2021-11-19 22:06:41 -0500

    add drawer to display buttons for archive/settings

--- src/Components/CallItem/CallItem.jsx
+++ src/Components/CallItem/CallItem.jsx
@@ -1,5 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faArchive,
+  faEllipsisV,
+  faEnvelope,
+  faInfoCircle,
+  faPhoneAlt,
+} from "@fortawesome/free-solid-svg-icons";
 
 import "./CallItem.css";
 import outboundCall from "../../../public/images/outbound.png";
@@ -13,7 +22,7 @@ import Time from "../UI/Time/Time.jsx";
 
 const CallItem = (props) => {
   const { id, created_at, direction, from, to, count } = props;
-
+  const [displayDrawer, setDisplayDrawer] = useState(false);
   const realDate = new Date(created_at);
   const callDate = format(realDate, "MMMM, dd yyyy", {
     locale: enCALocale,
@@ -25,35 +34,73 @@ const CallItem = (props) => {
   return (
     <div className="call_item" id={id}>
       <DateHeader date={callDate}></DateHeader>
-      <Link to={`/call/${id}`}>
-        <Container>
-          <img
-            className="call_item__icon-outbound"
-            src={direction === "outbound" ? outboundCall : inboundCall}
-          />
-          <div className="call_item__details">
-            <span>
-              <div style={{ display: "flex" }}>
-                <p style={{ marginRight: "4px" }}> {from} </p>
-                <div
-                  className={`${
-                    count <= 1 && "call_item__badge-display"
-                  } call_item__badge`}
-                >
-                  <span className="call_item__count">
-                    {count > 1 ? count : ""}
-                  </span>
-                </div>
+      {/* <Link to={`/call/${id}`}> */}
+      <div
+        onClick={() => {
+          setDisplayDrawer((prev) => !prev);
+        }}
+      >
+        <Container className={`${displayDrawer ? "display_drawer" : ""}`}>
+          <div className="test">
+            <div className="centered">
+              <img
+                className="call_item__icon-outbound"
+                src={direction === "outbound" ? outboundCall : inboundCall}
+              />
+              <div className="call_item__details">
+                <span>
+                  <div style={{ display: "flex" }}>
+                    <p style={{ marginRight: "4px" }}> {from} </p>
+                    <div
+                      className={`${
+                        count <= 1 && "call_item__badge-display"
+                      } call_item__badge`}
+                    >
+                      <span className="call_item__count">
+                        {count > 1 ? count : ""}
+                      </span>
+                    </div>
+                  </div>
+                  <p>
+                    <span className="call_item_light-span">
+                      tried to call on
+                    </span>{" "}
+                    {to === null ? "Private Number" : to}
+                  </p>
+                </span>
               </div>
-              <p>
-                <span className="call_item_light-span">tried to call on</span>{" "}
-                {to === null ? "Private Number" : to}
-              </p>
-            </span>
+              <Time time={callTime} date={realDate}></Time>
+            </div>
           </div>
-          <Time time={callTime} date={realDate}></Time>
+
+          <div className={`drawer ${!displayDrawer && "hide_drawer"}`}>
+            <p>Add to contacts</p>
+            <div className="icons">
+              <FontAwesomeIcon
+                size="2x"
+                color="#1eb91e"
+                icon={faPhoneAlt}
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                size="2x"
+                color="#2e6fb5"
+                icon={faEnvelope}
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                color="#5a5a5a"
+                size="2x"
+                icon={faArchive}
+              ></FontAwesomeIcon>
+              <FontAwesomeIcon
+                size="2x"
+                color="#2e6fb5"
+                icon={faInfoCircle}
+              ></FontAwesomeIcon>
+            </div>
+          </div>
         </Container>
-      </Link>
+        {/* </Link> */}
+      </div>
     </div>
   );
 };
--- src/Components/UI/Container/Container.jsx
+++ src/Components/UI/Container/Container.jsx
@@ -4,7 +4,13 @@ import "./Container.css";
 
 const Container = (props) => {
   return (
-    <div id={props.id} onClick={props.onClick} className="call_item__container">
+    <div
+      id={props.id}
+      onClick={props.onClick}
+      className={`call_item__container ${
+        props.className.length ? props.className : "container_hide_drawer"
+      }`}
+    >
       {props.children}
     </div>
   );
